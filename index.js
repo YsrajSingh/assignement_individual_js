005cd868f72971530ac76a48f3cd1fda4066647c
ysraz <ysgahir1925@hotmail.com>
2023-07-28 22:47:36 +0530

    update code for 2nd student :: Contact Manager App

--- index.js
+++ index.js
@@ -1,39 +1,60 @@
-// index.js
-const dataInput = document.getElementById("dataInput");
-const saveBtn = document.getElementById("saveBtn");
-const displayElement_1 = document.querySelector(".form");
-const displayElement_2 = document.querySelector(".welcome");
-const navbar = document.querySelector(".navbar");
+function displayContacts(all_contacts) {
+    const tableBody = document.querySelector("#contact_list tbody");
+    tableBody.innerHTML = "";
 
-function dataFetcher() {
-    const savedData = localStorage.getItem("user_name");
-    return savedData;
+    if (all_contacts.length === 0) {
+        const newRow = tableBody.insertRow();
+        const emptyCell = newRow.insertCell();
+        emptyCell.textContent = "There are no contacts to display";
+    } else {
+        all_contacts.forEach((each_contact) => {
+            const newRow = tableBody.insertRow();
+            const contact_id_field = newRow.insertCell();
+            const contact_name_field = newRow.insertCell();
+            const contact_update_field = newRow.insertCell();
+            const contact_delete_field = newRow.insertCell();
+            contact_id_field.textContent = each_contact.id;
+
+            const contact_link = document.createElement("a");
+            contact_link.textContent = each_contact.data_contact_name;
+            contact_link.href = `details.html?id=${each_contact.id}`;
+            contact_name_field.appendChild(contact_link);
+
+            contact_update_field.innerHTML = `<button onclick={window.location.href='/update.html?id=${each_contact.id}'} id='update_button' >Update Contact</button>`;
+            contact_delete_field.innerHTML =
+                "<button id='delete_button'>Delete Contact</button>";
+        });
+    }
 }
 
-saveBtn.addEventListener("click", () => {
-    const data = dataInput.value;
-    if (data.trim() !== "") {
-        localStorage.setItem("user_name", data);
-        dataInput.value = "";
+function getPreviousContacts() {
+    const jsonContacts = localStorage.getItem("contact_list");
+    return jsonContacts ? JSON.parse(jsonContacts) : [];
+}
 
-        // Reload the page to display the welcome message
-        window.location.reload();
-    }
-});
+function saveContactsToLocalStorage(contact_list) {
+    localStorage.setItem("contact_list", JSON.stringify(contact_list));
+}
 
 document.addEventListener("DOMContentLoaded", () => {
-    const authenticated = dataFetcher();
-    // If not authenticated, hide the welcome message, show the login form, and return
-    if (!authenticated) {
-        // window.location.replace("/");
-        navbar.style.display = "none";
-        displayElement_1.style.display = "block";
-        displayElement_2.style.display = "none";
-        return; // Stop further execution
-    }
+    const contactArray = getPreviousContacts();
+    displayContacts(contactArray);
 
-    // If authenticated, hide the login form and show the welcome message
-    displayElement_1.style.display = "none";
-    displayElement_2.innerHTML = `Welcome <b>${authenticated}</b>`;
-    displayElement_2.style.display = "block";
+    const deleteButtons = document.querySelectorAll("#delete_button");
+    console.log(deleteButtons);
+
+    deleteButtons.forEach((button, index) => {
+        console.log(button, index);
+        button.addEventListener("click", () => {
+            const contactIdToDelete = contactArray[index].id;
+
+            const updatedContactArray = contactArray.filter(
+                (contact) => contact.id !== contactIdToDelete
+            );
+
+            saveContactsToLocalStorage(updatedContactArray);
+
+            window.location.reload();
+        });
+    });
 });
--- logout.js
+++
@@ -1,7 +0,0 @@
-const resetButton = document.getElementById("logout");
-
-resetButton.addEventListener("click", () => {
-    localStorage.removeItem("user_name");
-    // Reload the page to show the login form again
-    window.location.href = "/";
-});
